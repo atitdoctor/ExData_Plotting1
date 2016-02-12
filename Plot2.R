714b3eb54265d24a8ea900dfb3c621eb6ff3ea47
Atit <atit.30187@gmail.com>
2016-02-12 03:09:49 -0800

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,6 +1,6 @@
 p <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
 p <- subset(p, Date == "1/2/2007" | Date == "2/2/2007")
 dt <- strptime(paste(p$Date, p$Time), format="%d/%m/%Y %H:%M:%S")
-png(filename="plot2.png")
+png(filename="Plot2.png")
 plot(x=dt, y=p[,"Global_active_power"], type="l",ylab="Global Active Power (kilowatts)", main="", xlab="")
 dev.off()
